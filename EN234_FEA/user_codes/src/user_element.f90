cd210965504ea87ef841bde3a64418e9d5957aec
RC-2016 <ryan_carlson@brown.edu>
2015-10-07 14:25:24 -0400

    Created el_linelast_2Dbasic

--- EN234_FEA/user_codes/src/user_element.f90
+++ EN234_FEA/user_codes/src/user_element.f90
@@ -65,14 +65,23 @@ subroutine user_element_static(lmn, element_identifier, n_nodes, node_property_l
     updated_state_variables,element_stiffness,element_residual, fail)      ! Output variables
 
 
-    else if ( element_identifier ==0) then           ! Stub for a new element
+    !  else if ( element_identifier ==0) then           ! Stub for a new element
   
-        call new_user_element_static(lmn, element_identifier, n_nodes, node_property_list, &           ! Input variables
+    !     call new_user_element_static(lmn, element_identifier, n_nodes, node_property_list, &           ! Input variables
+    !n_properties, element_properties, element_coords, length_coord_array, &                      ! Input variables
+    !dof_increment, dof_total, length_dof_array, &                                                ! Input variables
+    !n_state_variables, initial_state_variables, &                                                ! Input variables
+    !updated_state_variables,element_stiffness,element_residual, fail)      ! Output variables
+  
+  ! addition of new subroutines - RC 10/05/15
+    else if ( element_identifier == 101) then           ! Stub for a new element
+
+        call el_linelast_2Dbasic(lmn, element_identifier, n_nodes, node_property_list, &           ! Input variables
     n_properties, element_properties, element_coords, length_coord_array, &                      ! Input variables
     dof_increment, dof_total, length_dof_array, &                                                ! Input variables
     n_state_variables, initial_state_variables, &                                                ! Input variables
     updated_state_variables,element_stiffness,element_residual, fail)      ! Output variables
-  
+
   
     else
         write (IOW, 99001) element_identifier
@@ -224,7 +233,17 @@ subroutine user_element_fieldvariables(lmn, element_identifier, n_nodes, node_pr
             n_field_variables,field_variable_names, &                                                   ! Field variable definition
             nodal_fieldvariables)      ! Output variables
 
-        else if ( element_identifier == 0 ) then
+
+    else if ( element_identifier == 101 ) then              ! Basic fully integrated 2D linear elastic element
+
+        call fieldvars_linelast_2dbasic(lmn, element_identifier, n_nodes, node_property_list, &         ! Input variables
+            n_properties, element_properties,element_coords, length_coord_array,  &                     ! Input variables
+            dof_increment, dof_total, length_dof_array,  &                                              ! Input variables
+            n_state_variables, initial_state_variables,updated_state_variables, &                       ! Input variables
+            n_field_variables,field_variable_names, &                                                   ! Field variable definition
+            nodal_fieldvariables)      ! Output variables
+
+     else if ( element_identifier == 0 ) then
             call new_user_element_fieldvariables(lmn, element_identifier, n_nodes, node_property_list, &           ! Input variables
                 n_properties, element_properties,element_coords, length_coord_array, &                                   ! Input variables
                 dof_increment, dof_total, length_dof_array, &                                                            ! Input variables
